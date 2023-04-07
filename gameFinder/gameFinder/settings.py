d491ee2dba4f32bd95632cc2e3258c2e06e32d60
kpm2 <73690355+kpm2@users.noreply.github.com>
2023-04-07 03:04:12 +0200

    Add files via upload
    Canvi base de dades

--- gameFinder/gameFinder/settings.py
+++ gameFinder/gameFinder/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+import mysql.connector
+
+
+#Environ django
+env=Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +83,12 @@ WSGI_APPLICATION = 'gameFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nombre_de_tu_base_de_datos',
+        'USER': 'tu_usuario',
+        'PASSWORD': 'tu_contraseña',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
